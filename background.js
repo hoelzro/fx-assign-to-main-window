0b10f99604322a11cf18b1b05ea838905396f1b7
Rob Hoelz <rob@hoelz.ro>
2018-04-04 18:50:04 -0500

    Track redirects via onBeforeNavigate
    Normally if you receive a remote URL, the transition type will be
    "link", but the latest visit for that page will have a referringVisitId
    of -1, because you didn't actually click on a link to make that
    navigation happen.  One exception to this is redirects - if I send, say,
    https://ddg.gg (which redirects to https://duckduckgo.com), onCommitted
    only picks up the redirect destination, and the visit corresponding to
    the original URL is considered to be the referring visit.  So if we
    detect a redirect, we need to find out the visit that brought us here.
    
    To do that, we can subscribe to the onBeforeNavigate event, which
    gets the original URL.  We can track the previous URL in a table (keyed
    by tab, since redirects *should* happen within the same tab) and use
    that to look up the original visit to see if *it* has no
    referringVisitId

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+let previousBeforeNavigateByTab = {};
+
 browser.webNavigation.onCommitted.addListener(async (details) => {
     if(details.transitionType != 'link') {
         return;
@@ -6,13 +8,33 @@ browser.webNavigation.onCommitted.addListener(async (details) => {
         return;
     }
 
-    let visits = await browser.history.getVisits({
-        url: details.url
-    });
+    let wasRedirect = details.transitionQualifiers.includes('server_redirect');
+
+    let visits;
+
+    if(wasRedirect && details.tabId in previousBeforeNavigateByTab) {
+        visits = await browser.history.getVisits({
+            url: previousBeforeNavigateByTab[details.tabId]
+        });
+    } else {
+        visits = await browser.history.getVisits({
+            url: details.url
+        });
+    }
 
     if(visits.length == 0) {
         return;
     }
 
     let latestVisit = visits[0];
+
+    if(latestVisit.referringVisitId == -1) {
+        console.log('from external!');
+    }
 });
+
+browser.webNavigation.onBeforeNavigate.addListener((details) => {
+    previousBeforeNavigateByTab[details.tabId] = details.url;
+});
+
+// XXX clean up previousBeforeNavigateByTab
